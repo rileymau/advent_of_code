241be52568eb6008396abb4fa6e8103044eaac9b
Molly Riley <mo.riley@gmail.com>
2021-12-14 00:39:17 +0000

    finished part 2 for day 1

--- code.py
+++ code.py
@@ -11,27 +11,51 @@ txt = txt.read()
 
 # make list of data
 txt2 = txt.strip().split()
-print(txt2)
+txt3 = [int(x) for x in txt2]
+print(txt3)
 
-
-def make_counter():
+def make_counter(lst):
     """count increases line to line in input.txt"""
     counter = 0
     i = 0
 
-    # loop through 0 to length of txt2 - 1, 
+    # loop through 0 to length of txt2 - 1,
     # compare item with next item, counter counts number of increases.
-    while i < len(txt2) - 1:
-        if int(txt2[i]) < int(txt2[i + 1]):
+    while i < (len(lst) - 1):
+        if lst[i] < lst[i + 1]:
             counter += 1
         i += 1
     print(counter)
     return counter
 
-make_counter()
+make_counter(txt3)
 
 # got 1233
 
 
 ## Day 1 Part 2 ##
 
+# make new list of triples
+# append first item, first + second item, - don't need
+# then all triples until end
+# append last + second to last, last - don't need
+
+triples = []
+# triples.append(txt3[0])
+# triples.append(txt3[0] + txt3[1])
+
+j = 0
+while j < (len(txt3) - 2):
+    triples.append(txt3[j] + txt3[j + 1] + txt3[j + 2])
+    j += 1
+
+# triples.append(txt3[-2] + txt3[-1])
+# triples.append(txt3[-1])
+print(triples)
+
+# update make_counter function to take a lst
+# check list for increeases as in part 1
+
+make_counter(triples)
+
+# got 1275
